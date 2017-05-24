880b3213e06744d7c3fbe3c3531d73a76945fd3c
Shahbaz <jocnud@gmail.com>
2017-05-25 01:28:10 +0530

    Enabled eureka and config server

--- src/main/java/com/cloudsea/organization/OrganizationServiceApplication.java
+++ src/main/java/com/cloudsea/organization/OrganizationServiceApplication.java
@@ -2,6 +2,7 @@ package com.cloudsea.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication(scanBasePackages = { "com.cloudsea.*" })
 @EnableSwagger2
+@EnableDiscoveryClient
 @Import(value = { SpringDataRestConfiguration.class })
 public class OrganizationServiceApplication {
 
